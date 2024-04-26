8080fd07ec5ef5da31ae40c541218205282a35e9
shuyanglin <shuyanglin@Shus-MacBook-Pro.local>
2024-04-26 17:56:34 +0800

    color styling

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,11 +21,11 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -38,7 +38,7 @@ export async function fetchLatestInvoices() {
   noStore();
   try {
     console.log('Fetching invoice data...');
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -47,7 +47,7 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-    console.log('Inovice data fetch completed after 5 seconds.');
+    // console.log('Inovice data fetch completed after 5 seconds.');
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
